ef826082eba2e4dda9dd66a6f69b1af1c553cd95
Ivan Tarasiuk <viix345@gmail.com>
2023-11-13 20:50:49 +0100

    Ended AboutUs, SpecialMenu,Chef,Intro,Laurels, Gallery

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
-    {/* <Chef />
+    <Chef />
     <Intro />
     <Laurels />
     <Gallery />
-    <FindUs />
+    {/* <FindUs />
     <Footer /> */}
   </div>
 );
